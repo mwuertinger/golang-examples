366fd2dec147b28ca7b3531430b5edabc417cfd4
Michael Würtinger <michael.wuertinger@egym.de>
2017-03-22 16:15:42 +0100

    Go routines example

---
+++ go-routines/go-routines.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	count("foo")
+	count("bar")
+
+	// go count("foo")
+	// go count("bar")
+	// time.Sleep(6 * time.Second)
+}
+
+func count(name string) {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%s: %d\n", name, i)
+		time.Sleep(500 * time.Millisecond)
+	}
+}
